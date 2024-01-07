f2c21c27b4665cabf063dd9eac890f55fde24af6
terry <munu220@gmail.com>
2024-01-07 02:59:04 +0000

    delete: cut copyright comment

--- cogs/general.py
+++ cogs/general.py
@@ -1,11 +1,3 @@
-""""
-Copyright © Krypton 2019-2023 - https://github.com/kkrypt0nn (https://krypton.ninja)
-Description:
-🐍 A simple template to start to code your own and personalized discord bot in Python programming language.
-
-Version: 6.1.0
-"""
-
 import platform
 import random
 
