55ba9755977281febb74b1a8f9421cc36ed0f7f6
terry <munu220@gmail.com>
2024-01-09 11:26:56 +0000

    fix: advencement rank team generator

--- cogs/team.py
+++ cogs/team.py
@@ -4,53 +4,145 @@ from discord.ext import commands
 from discord.ext.commands import Context
 
 
-class Team(commands.Cog, name="team"):
+class TeamHandler:
+    LANE = ["탑", "정글", "미드", "원딜", "서폿"]
+
     def __init__(self, bot) -> None:
         self.bot = bot
-        self.base_weight = [[100 for _ in range(5)] for _ in range(5)]
-        self.rank_member_list = []
-        self.rank_history = []
+        self.multiple = 0.1
+        self.base_weight = [[10000 for _ in range(5)] for _ in range(5)]
+        self.messages: dict[int, discord.Message] = {}
+        self.teams: dict[int, list[discord.Member]] = {}
+        self.weights: dict[int, list[list[int]]] = {}
 
-    def get_rank_team(self) -> list[int]:
-        # 가중치 설정
-        self.rank_history[::-1]  # 최근 기록부터
-        for team in self.rank_history:
-            for member_no, lane_no in enumerate(team):  # 각 기록의 사람 마다
-                tmp = self.base_weight[member_no][lane_no] // 5
-                remain = self.base_weight[member_no][lane_no] % 5
-                for i in range(5):
-                    if i == lane_no:
-                        self.base_weight[member_no][i] = tmp + remain
-                    else:
-                        self.base_weight[member_no][i] += tmp
-        # 팀 뽑기
+    async def start_team(self, context: Context) -> None:
+        embed = discord.Embed(
+            title="새로운 팀이 생성되었습니다.",
+            description="**/rank join** 명령어를 통해 등록해 주세요",
+            color=0xBEBEFE,
+        )
+        embed.add_field(name="0/5", value="")
+        message = await context.send(embed=embed)
+
+        channel_id = message.channel.id
+        self.messages[channel_id] = message
+        self.teams[channel_id] = []
+        self.weights[channel_id] = self.base_weight.copy()
+
+        await self.join_team(context)
+
+    async def join_team(self, context: Context) -> None:
+        channel_id = context.channel.id
+        member = context.author
+
+        if channel_id not in self.teams:
+            await self.no_team_error(context)
+        if member in self.teams[channel_id]:
+            await self.already_in_team_error(context)
+        if len(self.teams[channel_id]) >= 5:
+            await self.max_team_error(context)
+
+        self.teams[channel_id].append(member)
+        await self.update_message(channel_id)
+
+        if len(self.teams[channel_id]) > 1:
+            embed = discord.Embed(
+                description=f"{member.mention}님이 팀에 등록되었습니다.",
+                color=0xBEBEFE,
+            )
+            await context.send(embed=embed)
+
+    async def update_message(self, channel_id: int) -> None:
+        if channel_id not in self.messages:
+            return
+        message = self.messages[channel_id]
+        embed = message.embeds[0]
+        embed.set_field_at(
+            index=0,
+            name=f"{len(self.teams[channel_id])}/5",
+            value=" - ".join([member.mention for member in self.teams[channel_id]]),
+        )
+        await message.edit(embed=embed)
+
+    async def shuffle_team(self, context: Context) -> None:
+        channel_id = context.channel.id
+
+        if channel_id not in self.teams:
+            await self.no_team_error(context)
+        if len(self.teams[channel_id]) != 5:
+            await self.team_member_num_error(context)
+
+        team = self.get_rank_team(channel_id)
+        self.adjustment_weight(channel_id, team)
+        embed = discord.Embed(
+            description="새로운 팀을 구성했습니다.",
+            color=0xBEBEFE,
+        )
+        for i, lane in enumerate(self.LANE):
+            member_no = team.index(i)
+            member = self.teams[channel_id][member_no]
+            embed.add_field(name=lane, value=member.mention)
+        await context.send(embed=embed)
+
+    def get_rank_team(self, channel_id: int) -> list[int]:
         team = []
         while len(set(team)) != 5:
             team.clear()
             for i in range(5):
                 team.append(
-                    random.choices(range(5), weights=self.base_weight[i], k=1)[0]
+                    random.choices(range(5), weights=self.weights[channel_id][i])[0]
                 )
         return team
 
-    def shuffle_rank_team(
-        self, member_list: list[discord.Member]
-    ) -> dict[str, discord.Member]:
-        team_base = self.get_rank_team()
-        self.rank_history.append(team_base)
-        team = {}
-        for i, t in enumerate(team_base):
-            if t == 0:
-                team["탑"] = member_list[i]
-            elif t == 1:
-                team["정글"] = member_list[i]
-            elif t == 2:
-                team["미드"] = member_list[i]
-            elif t == 3:
-                team["원딜"] = member_list[i]
-            elif t == 4:
-                team["서폿"] = member_list[i]
-        return team
+    def adjustment_weight(self, channel_id: int, team: list[int]) -> None:
+        weight = self.weights[channel_id].copy()
+        for member_no, lane_no in enumerate(team):
+            remain = (weight[member_no][lane_no] * (1 - self.multiple)) // 4
+            for i in range(5):
+                if i == lane_no:
+                    weight[member_no][i] -= remain * 4
+                else:
+                    weight[member_no][i] += remain
+        self.weights[channel_id] = weight
+
+    async def no_team_error(self, context: Context) -> None:
+        embed = discord.Embed(
+            title="팀이 생성되지 않았습니다.",
+            description="**/rank start** 팀을 생성해 주세요.",
+            color=0xE02B2B,
+        )
+        await context.send(embed=embed, ephemeral=True)
+        raise commands.CommandError("no rank team")
+
+    async def already_in_team_error(self, context: Context) -> None:
+        embed = discord.Embed(
+            description="이미 팀에 등록되었습니다.",
+            color=0xE02B2B,
+        )
+        await context.send(embed=embed, ephemeral=True)
+        raise commands.CommandError("already in rank team")
+
+    async def max_team_error(self, context: Context) -> None:
+        embed = discord.Embed(
+            description="팀이 꽉 찼습니다.",
+            color=0xE02B2B,
+        )
+        await context.send(embed=embed, ephemeral=True)
+        raise commands.CommandError("max rank team")
+
+    async def team_member_num_error(self, context: Context) -> None:
+        embed = discord.Embed(
+            description="인원이 맞지 않습니다.",
+            color=0xE02B2B,
+        )
+        await context.send(embed=embed, ephemeral=True)
+        raise commands.CommandError("team member num error")
+
+
+class Team(commands.Cog, name="team"):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+        self.handler = TeamHandler(bot)
 
     @commands.hybrid_group(
         name="rank",
@@ -59,52 +151,17 @@ class Team(commands.Cog, name="team"):
     async def rank(self, context: Context) -> None:
         pass
 
+    @rank.command(name="start", description="start rank team.")
+    async def rank_start(self, context: Context) -> None:
+        await self.handler.start_team(context)
+
+    @rank.command(name="join", description="join rank team.")
+    async def rank_join(self, context: Context) -> None:
+        await self.handler.join_team(context)
+
     @rank.command(name="shuffle", description="generate new rank team.")
     async def rank_base(self, context: Context) -> None:
-        if context.author.voice is None:
-            embed = discord.Embed(description="음성 채널에 접속해주세요.", color=0xBEBEFE)
-            await context.send(embed=embed, ephemeral=True)
-            return
-
-        member_list = context.author.voice.channel.members
-        member_list = member_list * 5
-        if len(member_list) == 5:
-            if not self.rank_member_list:
-                self.rank_member_list = member_list.copy()
-            else:
-                embed = discord.Embed(description="팀을 구성한 인원이 변경되었습니다.", color=0xBEBEFE)
-                embed.set_footer(text="팀을 초기화 해주세요.")
-                await context.send(embed=embed, ephemeral=True)
-                return
-            if self.rank_member_list == member_list:
-                team = self.shuffle_rank_team(member_list)
-                embed = discord.Embed(description="새로운 팀을 구성했습니다.", color=0xBEBEFE)
-                embed.add_field(name="탑", value=team["탑"].mention, inline=False)
-                embed.add_field(name="정글", value=team["정글"].mention, inline=False)
-                embed.add_field(name="미드", value=team["미드"].mention, inline=False)
-                embed.add_field(name="원딜", value=team["원딜"].mention, inline=False)
-                embed.add_field(name="서폿", value=team["서폿"].mention, inline=False)
-                await context.send(embed=embed)
-            return
-        elif len(member_list) < 5:
-            embed = discord.Embed(description="팀을 구성할 인원이 부족합니다.", color=0xBEBEFE)
-            embed.add_field(name="현재 인원", value=len(member_list))
-            embed.set_footer(text="채널에 5명만 있어야 합니다.")
-            await context.send(embed=embed, ephemeral=True)
-            return
-        else:
-            embed = discord.Embed(description="팀을 구성할 인원이 많습니다.", color=0xBEBEFE)
-            embed.add_field(name="현재 인원", value=len(member_list))
-            embed.set_footer(text="채널에 5명만 있어야 합니다.")
-            await context.send(embed=embed, ephemeral=True)
-            return
-
-    @rank.command(name="clear", description="clear rank history.")
-    async def rank_clear(self, context: Context) -> None:
-        self.rank_history.clear()
-        self.base_weight = [[100 for _ in range(5)] for _ in range(5)]
-        embed = discord.Embed(description="팀 기록을 초기화했습니다.", color=0xBEBEFE)
-        await context.send(embed=embed, ephemeral=True)
+        await self.handler.shuffle_team(context)
 
 
 async def setup(bot) -> None:
