d953993ee99c8cfd9bc11304aaa32d90d82af77a
terry <munu220@gmail.com>
2024-01-13 11:14:13 +0000

    fix: change speech style

--- cogs/team.py
+++ cogs/team.py
@@ -23,12 +23,12 @@ class TeamHandler:
     async def start(self, context: Context) -> None:
         channel_id = context.channel.id
         embed = discord.Embed(
-            title="새로운 팀이 생성되었습니다.",
-            description="**/j** 명령어를 통해 등록해 주세요",
+            title="새로운 팀이 구성되었어요.",
+            description="**/j**로 등록... 하든가 말든가",
             color=0xBEBEFE,
         )
         embed.add_field(name="0/5", value="")
-        embed.set_footer(text="/s로 팀을 구성합니다. /c로 팀 등록을 취소합니다.")
+        embed.set_footer(text="/s로 굴릴 수 있어요. /c로 팀 등록을 취소할 수 있어요.")
         message = await context.send(embed=embed)
         try:
             await self.db.start_team(channel_id, message.id)
@@ -36,8 +36,8 @@ class TeamHandler:
         except Exception as e:
             self.logger.error(e)
             embed = discord.Embed(
-                title="팀 생성에 실패했습니다.",
-                description="다시 시도해 주세요.",
+                title="팀 생성에 실패했어요.",
+                description="조금 쉬어야 할 것 같아요.",
                 color=0xE02B2B,
             )
             await message.edit(embed=embed)
@@ -65,7 +65,7 @@ class TeamHandler:
         await self.update_message(channel_id)
 
         embed = discord.Embed(
-            description=f"{(await self.db.get_message(channel_id)).jump_url}에 {member.mention}님이 팀에 등록되었습니다.",
+            description=f"{(await self.db.get_message(channel_id)).jump_url}에 {member.mention}님을 등록했어요.",
             color=0xBEBEFE,
         )
         await context.send(embed=embed)
@@ -83,7 +83,7 @@ class TeamHandler:
         await self.update_message(channel_id)
 
         embed = discord.Embed(
-            description=f"{member.mention}님이 팀 등록을 취소했습니다.",
+            description=f"{member.mention}님이 팀 등록을 취소했어요.",
             color=0xBEBEFE,
         )
         await context.send(embed=embed)
@@ -119,7 +119,7 @@ class TeamHandler:
         team = self.get_rank_team(channel_id)
         self.adjustment_weight(channel_id, team)
         embed = discord.Embed(
-            description="새로운 팀을 구성했습니다.",
+            description="라인을 배정했어요.",
             color=0xBEBEFE,
         )
         for i, lane in enumerate(self.LANE):
@@ -134,7 +134,7 @@ class TeamHandler:
         members = await self.db.get_members(channel_id)
         random.shuffle(members)
         embed = discord.Embed(
-            description="새로운 팀을 구성했습니다.",
+            description="새로운 대전을 구성했어요.",
             color=0xBEBEFE,
         )
         embed.add_field(
@@ -183,8 +183,8 @@ class TeamHandler:
 
     async def no_team_error(self, context: Context) -> None:
         embed = discord.Embed(
-            title="팀이 생성되지 않았습니다.",
-            description="**/q** 팀을 생성해 주세요.",
+            title="팀이 생성되지 않았어요.",
+            description="**/q**로 팀을 먼저 생성해 주세요.",
             color=0xE02B2B,
         )
         await context.send(embed=embed, ephemeral=True)
@@ -192,7 +192,7 @@ class TeamHandler:
 
     async def already_in_team_error(self, context: Context) -> None:
         embed = discord.Embed(
-            description="이미 팀에 등록되었습니다.",
+            description="이미 팀에 등록되어있어요.",
             color=0xE02B2B,
         )
         await context.send(embed=embed, ephemeral=True)
@@ -200,7 +200,7 @@ class TeamHandler:
 
     async def already_not_in_team_error(self, context: Context) -> None:
         embed = discord.Embed(
-            description="팀에 등록되어 있지 않습니다.",
+            description="팀에 등록되어 있지 않아요.",
             color=0xE02B2B,
         )
         await context.send(embed=embed, ephemeral=True)
@@ -208,7 +208,7 @@ class TeamHandler:
 
     async def max_team_error(self, context: Context) -> None:
         embed = discord.Embed(
-            description="팀이 꽉 찼습니다.",
+            description="팀이 꽉 찼어요.",
             color=0xE02B2B,
         )
         await context.send(embed=embed, ephemeral=True)
