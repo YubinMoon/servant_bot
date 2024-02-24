687e4a2c069f6425fa03abd928fd9f7fde645710
terry <munu220@gmail.com>
2024-02-24 11:15:26 +0000

    refactor: main bot class to sub file

---
+++ bot.py
@@ -0,0 +1,191 @@
+import os
+import platform
+import random
+import sys
+import traceback
+
+import discord
+import redis
+from discord.ext import commands, tasks
+from discord.ext.commands import Context
+
+from utils.logger import get_logger
+
+
+class ServantBot(commands.Bot):
+    def __init__(self, intents: discord.Intents, config) -> None:
+        super().__init__(
+            command_prefix=commands.when_mentioned_or(config["prefix"]),
+            intents=intents,
+            help_command=None,
+        )
+        """
+        This creates custom bot variables so that we can access these variables in cogs more easily.
+
+        For example, The config is available using the following code:
+        - self.config # In this class
+        - bot.config # In this file
+        - self.bot.config # In cogs
+        """
+        self.logger = get_logger("servant_bot")
+        self.config = config
+
+    async def load_db(self) -> None:
+        self.database = redis.Redis(
+            host=os.getenv("REDIS_HOST"),
+            port=os.getenv("REDIS_PORT"),
+            decode_responses=False,
+        )
+        try:
+            self.database.ping()
+        except:
+            self.logger.error("Failed to connect to the Redis server")
+            self.logger.debug(traceback.format_exc())
+            sys.exit(1)
+
+    async def load_cogs(self) -> None:
+        """
+        The code in this function is executed whenever the bot will start.
+        """
+        for file in os.listdir("cogs"):
+            if file.endswith(".py"):
+                extension = file[:-3]
+                try:
+                    await self.load_extension(f"cogs.{extension}")
+                    self.logger.info(f"Loaded extension '{extension}'")
+                except Exception as e:
+                    exception = f"{type(e).__name__}: {e}"
+                    self.logger.error(
+                        f"Failed to load extension {extension}\n{exception}"
+                    )
+
+    @tasks.loop(minutes=1.0)
+    async def status_task(self) -> None:
+        """
+        Setup the game status task of the bot.
+        """
+        statuses = self.get_status_list()
+        await self.change_presence(activity=discord.Game(random.choice(statuses)))
+
+    def get_status_list(self) -> list[str]:
+        try:
+            with open("status.txt", "r") as f:
+                return f.read().split("\n")
+        except FileNotFoundError:
+            self.logger.warning("Status file not found, using default status")
+            return ["limeskin"]
+
+    @status_task.before_loop
+    async def before_status_task(self) -> None:
+        """
+        Before starting the status changing task, we make sure the bot is ready
+        """
+        await self.wait_until_ready()
+
+    async def setup_hook(self) -> None:
+        """
+        This will just be executed when the bot starts the first time.
+        """
+        self.logger.info(f"Logged in as {self.user.name}")
+        self.logger.info(f"discord.py API version: {discord.__version__}")
+        self.logger.info(f"Python version: {platform.python_version()}")
+        self.logger.info(
+            f"Running on: {platform.system()} {platform.release()} ({os.name})"
+        )
+        self.logger.info("-------------------")
+        await self.load_db()
+        await self.load_cogs()
+        self.status_task.start()
+
+    async def on_ready(self) -> None:
+        self.logger.info("Sync starting...")
+        await self.tree.sync()
+        self.logger.info("Sync complete")
+
+    async def on_message(self, message: discord.Message) -> None:
+        """
+        The code in this event is executed every time someone sends a message, with or without the prefix
+
+        :param message: The message that was sent.
+        """
+        if message.author == self.user or message.author.bot:
+            return
+        await self.process_commands(message)
+
+    async def on_command_completion(self, context: Context) -> None:
+        """
+        The code in this event is executed every time a normal command has been *successfully* executed.
+
+        :param context: The context of the command that has been executed.
+        """
+        full_command_name = context.command.qualified_name
+        split = full_command_name.split(" ")
+        executed_command = str(split[0])
+        if context.guild is not None:
+            self.logger.info(
+                f"Executed {executed_command} command in {context.guild.name} (ID: {context.guild.id}) by {context.author} (ID: {context.author.id})"
+            )
+        else:
+            self.logger.info(
+                f"Executed {executed_command} command by {context.author} (ID: {context.author.id}) in DMs"
+            )
+
+    async def on_command_error(self, context: Context, error) -> None:
+        """
+        The code in this event is executed every time a normal valid command catches an error.
+
+        :param context: The context of the normal command that failed executing.
+        :param error: The error that has been faced.
+        """
+        if isinstance(error, commands.CommandOnCooldown):
+            minutes, seconds = divmod(error.retry_after, 60)
+            hours, minutes = divmod(minutes, 60)
+            hours = hours % 24
+            embed = discord.Embed(
+                description=f"**Please slow down** - You can use this command again in {f'{round(hours)} hours' if round(hours) > 0 else ''} {f'{round(minutes)} minutes' if round(minutes) > 0 else ''} {f'{round(seconds)} seconds' if round(seconds) > 0 else ''}.",
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed, ephemeral=True)
+        elif isinstance(error, commands.NotOwner):
+            embed = discord.Embed(
+                description="You are not the owner of the bot!", color=0xE02B2B
+            )
+            await context.send(embed=embed, ephemeral=True)
+            if context.guild:
+                self.logger.warning(
+                    f"{context.author} (ID: {context.author.id}) tried to execute an owner only command in the guild {context.guild.name} (ID: {context.guild.id}), but the user is not an owner of the bot."
+                )
+            else:
+                self.logger.warning(
+                    f"{context.author} (ID: {context.author.id}) tried to execute an owner only command in the bot's DMs, but the user is not an owner of the bot."
+                )
+        elif isinstance(error, commands.MissingPermissions):
+            embed = discord.Embed(
+                description="You are missing the permission(s) `"
+                + ", ".join(error.missing_permissions)
+                + "` to execute this command!",
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed, ephemeral=True)
+        elif isinstance(error, commands.BotMissingPermissions):
+            embed = discord.Embed(
+                description="I am missing the permission(s) `"
+                + ", ".join(error.missing_permissions)
+                + "` to fully perform this command!",
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed, ephemeral=True)
+        elif isinstance(error, commands.MissingRequiredArgument):
+            embed = discord.Embed(
+                title="Error!",
+                # We need to capitalize because the command arguments have no capital letter in the code and they are the first word in the error message.
+                description=str(error).capitalize(),
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed, ephemeral=True)
+        elif isinstance(error, commands.errors.CommandNotFound):
+            self.logger.warning(
+                f"{context.author} (ID: {context.author.id}) tried to execute the invalid command '{context.invoked_with}'"
+            )
+        else:
+            raise error
