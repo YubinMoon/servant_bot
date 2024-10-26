688b9b363277887553bc2e429b0ab2a35feeb609
terry <munu220@gmail.com>
2024-10-26 12:31:53 +0000

    설정 파일 변경

--- config.py
+++ config.py
@@ -28,30 +28,21 @@ class Config:
             with open(_config_file, "r") as f:
                 stored_config = json.load(f)
 
-            class_config = {
-                k: v
-                for k, v in self.__class__.__dict__.items()
-                if not k.startswith("_") and not callable(v)
-            }
+            for k, v in stored_config.items():
+                setattr(self, k, v)
 
-            if class_config != stored_config:
-                self._update_config(class_config)
         else:
             self._update_config()
 
-    def _update_config(self, config=None):
-        if config is None:
-            config = {
-                k: v
-                for k, v in self.__class__.__dict__.items()
-                if not k.startswith("_") and not callable(v)
-            }
+    def _update_config(self):
+        config = {
+            k: v
+            for k, v in self.__class__.__dict__.items()
+            if not k.startswith("_") and not callable(v)
+        }
 
         with open(_config_file, "w") as f:
             json.dump(config, f, indent=4)
-
-        for key, value in config.items():
-            setattr(self, key, value)
 
     def __getitem__(self, key):
         return self.__class__.__dict__[key]
