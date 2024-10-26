eacc6abe2faa7939a0c9f6f4513c039742b1af7d
terry <munu220@gmail.com>
2024-10-26 10:21:00 +0000

    감시 로직 업데이트

--- cogs/cog_monitor/__init__.py
+++ cogs/cog_monitor/__init__.py
@@ -1,15 +1,9 @@
-import os
-import platform
-from datetime import datetime
-from time import time
+from datetime import datetime, timedelta
 
 import discord
-from discord import Guild, app_commands
+from discord import Embed, Guild, TextChannel, app_commands
 from discord.ext import commands
 from discord.ext.commands import Context
-from langchain_core.documents import Document
-from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage
-from langchain_core.prompts.chat import BaseChatPromptTemplate
 
 from bot import ServantBot
 from config import config
@@ -23,6 +17,12 @@ class Monitor(commands.Cog, name="monitor"):
         self.bot = bot
 
         self._restrict = False
+
+        self.target_id = int(config.monitor.get("id", "0"))
+        self.target_name = config.monitor.get("name", "")
+        self.channel_id = int(config.monitor.get("channel", "0"))
+        self.target_guild: dict[int, dict] = {}
+
         self._start_time_val = datetime.now()
         self._start_time_lol = datetime.now()
 
@@ -37,114 +37,82 @@ class Monitor(commands.Cog, name="monitor"):
         ]
     )
     async def monitoring(self, context: Context, onoff: str) -> None:
-        user_id = int(config.monitor.get("id", "0"))
+        user_id = self.target_id
         if user_id == 0:
             await context.send("몰컴 감시 대상이 설정되지 않았습니다.", ephemeral=True)
+            return
         user = context.guild.get_member(user_id)
         if not user:
             await context.send("몰컴 감시 대상을 찾을 수 없습니다.", ephemeral=True)
+            return
 
         if onoff == "on":
-            self._restrict = True
+            self.target_guild[context.guild.id] = {}
             await context.send(f"{user.mention} 몰컴 감시기 가동", ephemeral=True)
         elif onoff == "off":
-            self._restrict = False
+            self.target_guild.pop(context.guild.id, None)
             await context.send(f"{user.mention} 몰컴 감시기 중지", ephemeral=True)
         else:
             await context.send("잘못된 입력입니다.")
 
     @commands.Cog.listener()
     async def on_presence_update(self, before: discord.Member, after: discord.Member):
-        user_id = int(config.monitor.get("id", "0"))
-        channel_id = int(config.monitor.get("channel", "0"))
-        guild_id = int(config.monitor.get("guild", "0"))
-
         guild = before.guild
-        user = guild.get_member(user_id)
-        channel = self.get_channel(guild, channel_id)
-        first_channel = guild.text_channels[0]
-        test_channel = guild.text_channels[-1]
+        user = guild.get_member(self.target_id)
+        channel = self.get_channel(guild, self.channel_id)
 
-        if guild.id == int(os.getenv("PJY_GUILD_ID")):
-            logger.info(f"user: {after.display_name}")
-            logger.info(f"{before.activity} -> {after.activity}")
-            if after.id == int(os.getenv("PJY_ID")):
-                logger.info("타켓 확인")
-                if (
-                    before.desktop_status != after.desktop_status
-                    and after.desktop_status == discord.Status.online
-                ):
-                    logger.info(f"{os.getenv('PJY_NAME')} 컴퓨터 온라인 검거")
-                    if self._restrict:
-                        await test_channel.send(
-                            f"{os.getenv('PJY_NAME')} 컴퓨터 온라인 검거.", silent=True
-                        )
+        if guild.id in self.target_guild and before.id == user.id:
+            data = self.target_guild[guild.id]
+            if before.desktop_status != after.desktop_status:
+                if after.desktop_status == discord.Status.online:
+                    logger.info(f"{self.target_name} 컴퓨터 온라인 검거")
+                    data["start_time"] = datetime.now()
+                    data["status"] = "online"
+                elif after.desktop_status == discord.Status.offline:
+                    logger.info(f"{self.target_name} 컴퓨터 오프라인 검거")
+                    if data["status"] == "online":
+                        data["end_time"] = datetime.now()
+                        if data.get("alerted", False):
+                            await self.alert_end(channel)
 
-                before_activity = (
-                    "" if before.activity is None else (before.activity.name or "")
-                )
-                after_activity = (
-                    "" if after.activity is None else (after.activity.name or "")
-                )
-                if before_activity != after_activity:
-                    if "VALORANT" in after_activity:
-                        if after.voice is None:
-                            logger.info(f"{os.getenv('PJY_NAME')} 몰로란트 시작")
-                            if self._restrict:
-                                self._start_time_val = datetime.now()
-                                await first_channel.send(
-                                    "WARNING! WARNING! WARNING!\n"
-                                    f"{after.mention}몰로란트 검거\n"
-                                    "WARNING! WARNING! WARNING!"
-                                )
-                    elif "League of Legends" in after_activity:
-                        if after.voice is None:
-                            logger.info(f"{os.getenv('PJY_NAME')} 몰롤 시작")
-                            if self._restrict:
-                                self._start_time_lol = datetime.now()
-                                await first_channel.send(
-                                    "WARNING! WARNING! WARNING!\n"
-                                    f"{after.mention}몰롤 검거\n"
-                                    "WARNING! WARNING! WARNING!"
-                                )
-                    elif "VALORANT" in before_activity and after_activity == "":
-                        if after.voice is None:
-                            logger.info(f"{os.getenv('PJY_NAME')} 게임 종료")
-                            if self._restrict:
-                                _time = datetime.now() - self._start_time_val
-                                seconds = _time.seconds
-                                minute = seconds // 60
-                                time_format = "몰컴 시간: "
-                                if minute:
-                                    time_format += f"{minute}분 "
-                                time_format += f"{seconds % 60}초"
+            if before.activity == None or after.activity != None:
+                if data["status"] == "online" and not data.get("alerted", False):
+                    logger.info(f"{self.target_name} 몰컴 검거")
+                    await self.alert_start(channel)
+                    data["alerted"] = True
 
-                                await first_channel.send(
-                                    "WARNING! WARNING! WARNING!\n"
-                                    f"{after.mention}몰로란트 종료\n"
-                                    f"{time_format}\n"
-                                    "WARNING! WARNING! WARNING!"
-                                )
-                    elif (
-                        "League of Legends" in before_activity and after_activity == ""
-                    ):
-                        if after.voice is None:
-                            logger.info(f"{os.getenv('PJY_NAME')} 게임 종료")
-                            if self._restrict:
-                                _time = datetime.now() - self._start_time_lol
-                                seconds = _time.seconds
-                                minute = seconds // 60
-                                time_format = "몰컴 시간: "
-                                if minute:
-                                    time_format += f"{minute}분 "
-                                time_format += f"{seconds % 60}초"
+            # save changed data
+            self.target_guild[guild.id] = data
 
-                                await first_channel.send(
-                                    "WARNING! WARNING! WARNING!\n"
-                                    f"{after.mention}몰롤 종료\n"
-                                    f"{time_format}\n"
-                                    "WARNING! WARNING! WARNING!"
-                                )
+    async def alert_start(self, channel: TextChannel):
+        await channel.send(
+            "WARNING! WARNING! WARNING!\n"
+            f"{self.target_name} 몰컴 검거\n"
+            "WARNING! WARNING! WARNING!"
+        )
+
+    async def alert_end(self, channel: TextChannel, data):
+        start_date = data["start_time"].strftime("%m/%d %H:%M")
+        end_date = data["end_time"].strftime("%m/%d %H:%M")
+        duration = data["end_time"] - data["start_time"]
+
+        def strfdelta(tdelta: timedelta):
+            hour = tdelta.seconds // 3600
+            minute = (tdelta.seconds % 3600) // 60
+            result = ""
+            if hour:
+                result += f"{hour}시간"
+            if minute:
+                result += f" {minute}분"
+            return result
+
+        await channel.send(
+            "WARNING! WARNING! WARNING!\n"
+            f"{self.target_name} 몰컴 종료\n"
+            f"{start_date} ~ {end_date}\n"
+            f"{strfdelta(duration)} 동안 몰컴 완료!\n"
+            "WARNING! WARNING! WARNING!"
+        )
 
     def get_channel(self, guild: Guild, channel_id: int):
         channel = guild.get_channel(channel_id) or guild.text_channels[0]
--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
 
     monitor = {
         "id": "",
-        "guild": "",
+        "name": "",
         "channel": "",
     }
 
